2073ceb055161acfbf6610cf6349635eca17f39f
PeterFavero <peterfavero10@gmail.com>
2023-11-19 16:46:56 -0500

    Changed run.py so that DataLoading is only run if data isn't already loaded, and DataPreprocessing is only run if data isn't already preprocessed.

--- src/run.py
+++ src/run.py
@@ -1,4 +1,13 @@
-import DataLoading
-import DataPreprocessing
+import os
+
+#Run DataLoading only if loaded is empty
+if(os.listdir('loaded') == []) : import DataLoading
+
+#Run DataPreprocessing only if preprocessed is empty
+if(os.listdir('preprocessed') == []) : import DataPreprocessing
+
+#Run ModelTraining
 import ModelTraining
+
+#Run Visualizing
 import Visualizing
